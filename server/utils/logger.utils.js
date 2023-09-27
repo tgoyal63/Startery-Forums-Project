6f2a54d76b6657027cdb0c731d081efa8e088d7b
Tushar Goyal <tgoyal63@gmail.com>
2023-09-28 00:03:54 +0530

    Comment file logging code

--- server/utils/logger.utils.js
+++ server/utils/logger.utils.js
@@ -5,21 +5,21 @@ const path = require("path");
 const { isDevEnvironment, logDirectory } = require("../config/config");
 
 // Create log directory if it doesn't exist
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 // Setup winston logger
 const logger = winston.createLogger({
   level: isDevEnvironment ? "debug" : "info",
   format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: path.join(logDirectory, "/error.log"),
-      level: "error",
-    }),
-    new winston.transports.File({
-      filename: path.join(logDirectory, "/combined.log"),
-    }),
-  ],
+  // transports: [
+  //   new winston.transports.File({
+  //     filename: path.join(logDirectory, "/error.log"),
+  //     level: "error",
+  //   }),
+  //   new winston.transports.File({
+  //     filename: path.join(logDirectory, "/combined.log"),
+  //   }),
+  // ],
 });
 
 // If we're not in production, log to the `console` with the format: `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
